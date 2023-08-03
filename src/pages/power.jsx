4c50fe97658b0831161ba4ed3aab3a68b5163bfc
gnanadeep <gnanadeep4@gmail.com>
2023-08-02 19:23:22 -0700

    update side nav

--- src/pages/power.jsx
+++ src/pages/power.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+import { Dialog, Disclosure, Transition } from '@headlessui/react'
 import {
   Bars3Icon,
   CalendarIcon,
@@ -12,10 +12,26 @@ import {
 } from '@heroicons/react/24/outline'
 import Content from '@/components/Content'
 import Link from 'next/link'
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
 
 const navigation = [
-  { name: 'The Journey Of Excellence', href: '#', icon: HomeIcon, current: true },
-  { name: 'The Business', href: '#business', icon: UsersIcon, current: false },
+  {
+    name: 'The Journey', href: '#', icon: HomeIcon, current: true,
+    children: [
+      { name: 'Engineering', href: '#', current: true },
+      { name: 'Human Resources', href: '#', current: false },
+      { name: 'Customer Success', href: '#', current: false },
+    ],
+  },
+  {
+    name: 'The Business', href: '#business', icon: UsersIcon, current: false,
+    children: [
+      { name: 'GraphQL API', href: '#', current: false },
+      { name: 'iOS App', href: '#', current: false },
+      { name: 'Android App', href: '#', current: false },
+      { name: 'New Customer Portal', href: '#', current: false },
+    ],
+  },
   { name: 'Vision & Mission', href: '#vision', icon: FolderIcon, current: false },
   { name: 'The Sanpec Edge', href: '#info', icon: CalendarIcon, current: false },
   { name: 'Core Values', href: '#core', icon: DocumentDuplicateIcon, current: false },
@@ -97,45 +113,62 @@ export default function Example() {
                           <ul role="list" className="-mx-2 space-y-1">
                             {navigation.map((item) => (
                               <li key={item.name}>
-                                <a
-                                  href={item.href}
-                                  className={classNames(
-                                    item.current
-                                      ? 'bg-gray-800 text-white'
-                                      : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                                    'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                  )}
-                                >
-                                  <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
-                                  {item.name}
-                                </a>
+                                {!item.children ? (
+                                  <a
+                                    href={item.href}
+                                    className={classNames(
+                                      item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
+                                      'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                                    )}
+                                  >
+                                    <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                                    {item.name}
+                                  </a>
+                                ) : (
+                                  <Disclosure as="div">
+                                    {({ open }) => (
+                                      <>
+                                        <Disclosure.Button
+                                          className={classNames(
+                                            item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
+                                            'flex items-center w-full text-left group gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                                          )}
+                                        >
+                                          <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                                          {item.name}
+                                          <ChevronRightIcon
+                                            className={classNames(
+                                              open ? 'rotate-90 text-gray-500' : 'text-gray-400',
+                                              'ml-auto h-5 w-5 shrink-0'
+                                            )}
+                                            aria-hidden="true"
+                                          />
+                                        </Disclosure.Button>
+                                        <Disclosure.Panel as="ul" className="mt-1 px-2">
+                                          {item.children.map((subItem) => (
+                                            <li key={subItem.name}>
+                                              {/* 44px */}
+                                              <Disclosure.Button
+                                                as="a"
+                                                href={subItem.href}
+                                                className={classNames(
+                                                  subItem.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
+                                                  'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-700'
+                                                )}
+                                              >
+                                                {subItem.name}
+                                              </Disclosure.Button>
+                                            </li>
+                                          ))}
+                                        </Disclosure.Panel>
+                                      </>
+                                    )}
+                                  </Disclosure>
+                                )}
                               </li>
                             ))}
                           </ul>
                         </li>
-                        {/* <li>
-                          <div className="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-                          <ul role="list" className="-mx-2 mt-2 space-y-1">
-                            {teams.map((team) => (
-                              <li key={team.name}>
-                                <a
-                                  href={team.href}
-                                  className={classNames(
-                                    team.current
-                                      ? 'bg-gray-800 text-white'
-                                      : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                                    'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                  )}
-                                >
-                                  <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                                    {team.initial}
-                                  </span>
-                                  <span className="truncate">{team.name}</span>
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </li> */}
                       </ul>
                     </nav>
                   </div>
@@ -164,59 +197,76 @@ export default function Example() {
                   <ul role="list" className="-mx-2 space-y-1">
                     {navigation.map((item) => (
                       <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? 'bg-gray-800 text-white'
-                              : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                            'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                          )}
-                        >
-                          <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
-                          {item.name}
-                        </a>
+                        {!item.children ? (
+                          <a
+                            href={item.href}
+                            className={classNames(
+                              item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
+                              'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                            )}
+                          >
+                            <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                            {item.name}
+                          </a>
+                        ) : (
+                          <Disclosure as="div">
+                            {({ open }) => (
+                              <>
+                                <Disclosure.Button
+                                  className={classNames(
+                                    item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
+                                    'flex items-center w-full text-left group gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                                  )}
+                                >
+                                  <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                                  {item.name}
+                                  <ChevronRightIcon
+                                    className={classNames(
+                                      open ? 'rotate-90 text-gray-500' : 'text-gray-400',
+                                      'ml-auto h-5 w-5 shrink-0'
+                                    )}
+                                    aria-hidden="true"
+                                  />
+                                </Disclosure.Button>
+                                <Disclosure.Panel as="ul" className="mt-1 px-2">
+                                  {item.children.map((subItem) => (
+                                    <li key={subItem.name}>
+                                      {/* 44px */}
+                                      <Disclosure.Button
+                                        as="a"
+                                        href={subItem.href}
+                                        className={classNames(
+                                          subItem.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
+                                          'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-700'
+                                        )}
+                                      >
+                                        {subItem.name}
+                                      </Disclosure.Button>
+                                    </li>
+                                  ))}
+                                </Disclosure.Panel>
+                              </>
+                            )}
+                          </Disclosure>
+                        )}
                       </li>
                     ))}
                   </ul>
                 </li>
-                {/* <li>
-                  <div className="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-                  <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
-                        <a
-                          href={team.href}
-                          className={classNames(
-                            team.current
-                              ? 'bg-gray-800 text-white'
-                              : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                            'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                          )}
-                        >
-                          <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                            {team.initial}
-                          </span>
-                          <span className="truncate">{team.name}</span>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li> */}
-                {/* <li className="-mx-6 mt-auto">
+                <li className="-mx-6 mt-auto">
                   <a
                     href="#"
-                    className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-gray-800"
+                    className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50"
                   >
                     <img
-                      className="h-8 w-8 rounded-full bg-gray-800"
+                      className="h-8 w-8 rounded-full bg-gray-50"
                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                       alt=""
                     />
                     <span className="sr-only">Your profile</span>
                     <span aria-hidden="true">Tom Cook</span>
                   </a>
-                </li> */}
+                </li>
               </ul>
             </nav>
           </div>
