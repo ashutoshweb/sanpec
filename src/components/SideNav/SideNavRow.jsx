71354d9460d75cdfc081ea490d70a0ed020518cc
gnanadeep <gnanadeep4@gmail.com>
2023-08-04 18:31:14 -0700

    refactor side nav

---
+++ src/components/SideNav/SideNavRow.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Disclosure } from '@headlessui/react'
+import {
+  ChevronRightIcon,
+} from '@heroicons/react/24/outline'
+
+export function SideNavRow({ classNames, selected, setSelected, item }) {
+  return (
+    <Disclosure>
+      {({ open }) => (
+        <>
+          <Disclosure.Button
+            className={classNames(
+              selected === item.name
+                ? 'bg-gray-800 text-white'
+                : 'text-gray-400 hover:text-white hover:bg-gray-800',
+              'flex items-center w-full text-left group gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+            )}
+            onClick={() => setSelected(item.name)}
+          >
+            <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+            {item.name}
+            <ChevronRightIcon
+              className={classNames(
+                open ? 'rotate-90 text-gray-500' : 'text-gray-400',
+                'ml-auto h-5 w-5 shrink-0'
+              )}
+              aria-hidden="true"
+            />
+          </Disclosure.Button>
+          <Disclosure.Panel as="ul" className="mt-1 px-2">
+            {item.children.map((subItem) => (
+              <li key={subItem.name}>
+                <a
+                  href={subItem.href}
+                  className={classNames(
+                    subItem.current
+                      ? 'bg-gray-800 text-white'
+                      : 'text-gray-400 hover:text-white hover:bg-gray-800',
+                    'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-700'
+                  )}
+                >
+                  {subItem.name}
+                </a>
+              </li>
+            ))}
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
+
+export default SideNavRow;
--- src/pages/power.jsx
+++ src/pages/power.jsx
@@ -1,5 +1,4 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Transition } from '@headlessui/react'
+import { Sidebar } from "@/components/SideNav/Sidebar";
 import {
   Bars3Icon,
   CalendarIcon,
@@ -11,269 +10,50 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import Content from '@/components/Content'
-import Link from 'next/link'
-import { ChevronRightIcon } from '@heroicons/react/20/solid'
 
 const navigation = [
   {
-    name: 'The Journey', href: '#', icon: HomeIcon, current: true,
+    id: 1,
+    name: 'The Journey',
+    href: '#',
+    icon: HomeIcon,
+    current: true,
     children: [
-      { name: 'Engineering', href: '#', current: true },
-      { name: 'Human Resources', href: '#', current: false },
-      { name: 'Customer Success', href: '#', current: false },
+      { id: 2, name: 'Engineering', href: '#', current: true },
+      { id: 3, name: 'Human Resources', href: '#', current: false },
+      { id: 4, name: 'Customer Success', href: '#', current: false },
     ],
   },
   {
-    name: 'The Business', href: '#business', icon: UsersIcon, current: false,
+    id: 5,
+    name: 'The Business',
+    href: '#business',
+    icon: UsersIcon,
+    current: false,
     children: [
-      { name: 'GraphQL API', href: '#', current: false },
-      { name: 'iOS App', href: '#', current: false },
-      { name: 'Android App', href: '#', current: false },
-      { name: 'New Customer Portal', href: '#', current: false },
+      { id: 6, name: 'GraphQL API', href: '#', current: false },
+      { id: 7, name: 'iOS App', href: '#', current: false },
+      { id: 8, name: 'Android App', href: '#', current: false },
+      { id: 9, name: 'New Customer Portal', href: '#', current: false },
     ],
   },
-  { name: 'Vision & Mission', href: '#vision', icon: FolderIcon, current: false },
-  { name: 'The Sanpec Edge', href: '#info', icon: CalendarIcon, current: false },
-  { name: 'Core Values', href: '#core', icon: DocumentDuplicateIcon, current: false },
-  { name: 'Social Impact', href: '#social', icon: ChartPieIcon, current: false },
-  { name: 'Contact', href: '#contact', icon: ChartPieIcon, current: false },
-]
-const teams = [
-  // { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
-  // { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
-  // { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
+  { id: 10, name: 'Vision & Mission', href: '#vision', icon: FolderIcon, current: false },
+  { id: 11, name: 'The Sanpec Edge', href: '#info', icon: CalendarIcon, current: false },
+  { id: 12, name: 'Core Values', href: '#core', icon: DocumentDuplicateIcon, current: false },
+  { id: 13, name: 'Social Impact', href: '#social', icon: ChartPieIcon, current: false },
+  { id: 14, name: 'Contact', href: '#contact', icon: ChartPieIcon, current: false },
 ]
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
-export default function Example() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
-
+const Power = () => {
   return (
-    <>
-
-      <div>
-        <Transition.Root show={sidebarOpen} as={Fragment}>
-          <Dialog as="div" className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="fixed inset-0 bg-gray-900/80" />
-            </Transition.Child>
-
-            <div className="fixed inset-0 flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="-translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="-translate-x-full"
-              >
-                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-in-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-300"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                      <button type="button" className="-m-2.5 p-2.5" onClick={() => setSidebarOpen(false)}>
-                        <span className="sr-only">Close sidebar</span>
-                        <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </Transition.Child>
-                  {/* Sidebar component, swap this element with another sidebar if you like */}
-                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10">
-                    <div className="flex h-16 shrink-0 items-center">
-                      <Link href="/">
-                        <img
-                          className="h-8 w-auto"
-                          src="https://cdn.firespring.com/images/e2e5a88a-f06b-43b7-9f5e-556a83a3d522.png"
-                          alt="Your Company"
-                        />
-                      </Link>
-                    </div>
-                    <nav className="flex flex-1 flex-col">
-                      <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                        <li>
-                          <ul role="list" className="-mx-2 space-y-1">
-                            {navigation.map((item) => (
-                              <li key={item.name}>
-                                {!item.children ? (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                                      'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                    )}
-                                  >
-                                    <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
-                                    {item.name}
-                                  </a>
-                                ) : (
-                                  <Disclosure as="div">
-                                    {({ open }) => (
-                                      <>
-                                        <Disclosure.Button
-                                          className={classNames(
-                                            item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                                            'flex items-center w-full text-left group gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                          )}
-                                        >
-                                          <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
-                                          {item.name}
-                                          <ChevronRightIcon
-                                            className={classNames(
-                                              open ? 'rotate-90 text-gray-500' : 'text-gray-400',
-                                              'ml-auto h-5 w-5 shrink-0'
-                                            )}
-                                            aria-hidden="true"
-                                          />
-                                        </Disclosure.Button>
-                                        <Disclosure.Panel as="ul" className="mt-1 px-2">
-                                          {item.children.map((subItem) => (
-                                            <li key={subItem.name}>
-                                              {/* 44px */}
-                                              <Disclosure.Button
-                                                as="a"
-                                                href={subItem.href}
-                                                className={classNames(
-                                                  subItem.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                                                  'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-700'
-                                                )}
-                                              >
-                                                {subItem.name}
-                                              </Disclosure.Button>
-                                            </li>
-                                          ))}
-                                        </Disclosure.Panel>
-                                      </>
-                                    )}
-                                  </Disclosure>
-                                )}
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
-                      </ul>
-                    </nav>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition.Root>
-
-        {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-          {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
-            <div className="flex h-16 shrink-0 items-center">
-              <Link href="/">
-                <img
-                  className="h-8 w-auto"
-                  src="https://cdn.firespring.com/images/e2e5a88a-f06b-43b7-9f5e-556a83a3d522.png"
-                  alt="Your Company"
-                />
-              </Link>
-            </div>
-            <nav className="flex flex-1 flex-col">
-              <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                <li>
-                  <ul role="list" className="-mx-2 space-y-1">
-                    {navigation.map((item) => (
-                      <li key={item.name}>
-                        {!item.children ? (
-                          <a
-                            href={item.href}
-                            className={classNames(
-                              item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                              'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                            )}
-                          >
-                            <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
-                            {item.name}
-                          </a>
-                        ) : (
-                          <Disclosure as="div">
-                            {({ open }) => (
-                              <>
-                                <Disclosure.Button
-                                  className={classNames(
-                                    item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                                    'flex items-center w-full text-left group gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                  )}
-                                >
-                                  <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
-                                  {item.name}
-                                  <ChevronRightIcon
-                                    className={classNames(
-                                      open ? 'rotate-90 text-gray-500' : 'text-gray-400',
-                                      'ml-auto h-5 w-5 shrink-0'
-                                    )}
-                                    aria-hidden="true"
-                                  />
-                                </Disclosure.Button>
-                                <Disclosure.Panel as="ul" className="mt-1 px-2">
-                                  {item.children.map((subItem) => (
-                                    <li key={subItem.name}>
-                                      {/* 44px */}
-                                      <Disclosure.Button
-                                        as="a"
-                                        href={subItem.href}
-                                        className={classNames(
-                                          subItem.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                                          'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-700'
-                                        )}
-                                      >
-                                        {subItem.name}
-                                      </Disclosure.Button>
-                                    </li>
-                                  ))}
-                                </Disclosure.Panel>
-                              </>
-                            )}
-                          </Disclosure>
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-
-              </ul>
-            </nav>
-          </div>
-        </div>
-
-        <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
-          <button type="button" className="-m-2.5 p-2.5 text-gray-400 lg:hidden" onClick={() => setSidebarOpen(true)}>
-            <span className="sr-only">Open sidebar</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-          <div className="flex-1 text-sm font-semibold leading-6 text-white">Dashboard</div>
-
-        </div>
-
-        <main className="py-10 lg:pl-72">
-          <div className="px-4 sm:px-6 lg:px-8">
-            <Content />
-          </div>
-        </main>
-      </div>
-    </>
+    <div>
+      <Sidebar navigation={navigation} >
+        <Content />
+      </Sidebar>
+    </div>
   )
 }
+
+export default Power;
+
+
