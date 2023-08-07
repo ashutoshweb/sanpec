26ae5f1a503fa9d43b84224c3a8a094ea9e8d8a6
surajp <70827039+sp1244@users.noreply.github.com>
2023-08-06 17:13:50 -0700

    update power.jsx links

--- src/pages/power.jsx
+++ src/pages/power.jsx
@@ -20,33 +20,17 @@ const navigation = [
     icon: HomeIcon,
     current: true,
     children: [
-      {
-        id: 2, name: 'Design and Engineering', href: '#',
-        component: 'name', current: true
-      },
-      {
-        id: 3, name: 'Asset Management', href: '#',
-        component: 'name', current: false
-      },
-      {
-        id: 5, name: 'Tower Testing', href: '#',
-        component: 'name', current: false
-      },
-      {
-        id: 6, name: 'Material Take-off', href: '#',
-        component: 'name', current: false
-      },
-      {
-        id: 7, name: 'Drafting and Detailing', href: '#',
-        component: 'name', current: false
-      },
+      { id: 2, name: 'Design and Engineering', href: '#', current: true },
+      { id: 3, name: 'Asset Management', href: '#', current: false },
+      { id: 5, name: 'Tower Testing', href: '#', current: false },
+      { id: 6, name: 'Material Take-off', href: '#', current: false },
+      { id: 7, name: 'Drafting and Detailing', href: '#', current: false },
     ],
   },
   {
     id: 8,
     name: 'Manufacturing and Operations',
     href: '#',
-    component: 'name',
     icon: UsersIcon,
     current: false,
     children: [
@@ -64,14 +48,50 @@ const navigation = [
     current: false,
     children: [
       { id: 16, name: ' Material Expeditors', href: '#', current: false },
-      { id: 17, name: 'Source Inspection', href: '#', current: false },
-      { id: 18, name: 'Quality Controls', href: '#', current: false },
+      { id: 17, name: 'Logistic support', href: '#', current: false },
+      { id: 18, name: 'Yard Management', href: '#', current: false },
     ],
   },
-  { id: 11, name: 'The Sanpec Edge', href: '#info', icon: CalendarIcon, current: false, component: Content },
-  { id: 12, name: 'Core Values', href: '#core', icon: DocumentDuplicateIcon, current: false },
-  { id: 13, name: 'Social Impact', href: '#social', icon: ChartPieIcon, current: false, component: Content },
-  { id: 14, name: 'Contact', href: '#contact', icon: ChartPieIcon, current: false },
+  {
+    id: 11,
+    name: 'Constructability Review',
+    href: '#info',
+    icon: CalendarIcon,
+    current: false ,
+    children: [
+      { id: 161, name: 'IFC Package', href: '#', current: false },
+      { id: 171, name: 'Construction Support', href: '#', current: false },
+    ],
+  },
+  {
+    id: 12,
+    name: 'Dynamic Risk Management',
+    href: '#core',
+    icon: DocumentDuplicateIcon,
+    current: false,
+  },
+  {
+    id: 13,
+    name: 'Business And System Excellence',
+    href: '#social',
+    icon: ChartPieIcon,
+    current: false,
+    children: [
+      { id: 162, name: 'Business Excellence  ', href: '#', current: false },
+    ]
+  },
+  { id: 14,
+    name: 'Renewables',
+    href: '#contact',
+    icon: ChartPieIcon,
+    current: false,
+    children: [
+      { id: 163, name: 'Hydrogen', href: '#', current: false },
+      { id: 163, name: 'Wind And Solar', href: '#', current: false },
+      { id: 163, name: 'Smart Grid', href: '#', current: false },
+    ]
+
+  },
 ]
 
 const Power = () => {
