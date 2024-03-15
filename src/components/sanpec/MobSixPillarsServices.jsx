ff7a71528d2bda1ff2a7411c060c4878ec0515fa
Ashutosh <ashutoshid@yahoo.co.in>
2024-03-15 16:54:49 -0700

    Add Six pillar image

--- src/components/sanpec/MobSixPillarsServices.jsx
+++ src/components/sanpec/MobSixPillarsServices.jsx
@@ -2,7 +2,7 @@ import { Disclosure } from '@headlessui/react'
 import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline'
 import KnowledgeFeature from './features/Knowledge'
 
-
+import Image from 'next/image'
 
 
 import SystemsPerspective from "./sixpillars/SystemsPerspective";
@@ -11,6 +11,8 @@ import NewInnovation from "./sixpillars/NewInnovation";
 import ProcessExcellence from "./sixpillars/ProcessExcellence";
 import EcosystemInnovation from "./sixpillars/EcosystemInnovation";
 import Community from "./sixpillars/Community";
+
+import sanpecsixpillar from '@/custom-images/Sanpec-sixpillars.png'
 
 const faqs = [
   {
@@ -52,9 +54,20 @@ const faqs = [
 export  function MobSixPillarsServices() {
   return (
     <div>
-      <div className="mx-auto max-w-7xl px-6 py-12 sm:py-12 lg:px-8 lg:py-12">
+      <div className="mx-auto max-w-7xl px-6 py-12 sm:py-12 lg:px-0 lg:py-12">
         <div className="mx-auto max-w-4xl divide-y divide-white/10">
           <h2 className="text-3xl font-bold tracking-tight text-[#151949] sm:text-4xl text-center">SANPEC&apos;s Six Pillars of Excellence</h2>
+          <div
+              className="grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 lg:grid-cols-12 lg:pt-0 lg:pb-12"
+          >
+
+          </div>
+          <Image
+              className="w-full"
+              src={sanpecsixpillar}
+              alt=""
+              sizes="52.75rem"
+          />
           <dl className="mt-10 space-y-6 divide-y divide-gray-700/10">
             {faqs.map((faq) => (
               <Disclosure as="div" key={faq.question} className="pt-6">
